248e7ecf23b87ef85328ceab32e58142642d1b26
junhyun1001 <90903322+junhyun1001@users.noreply.github.com>
2023-08-10 12:00:31 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,42 +1,23 @@
-import java.util.HashSet;
-import java.util.Set;
-
+/*
+    유클리드 호제법을 사용하여 최대공약수를 구할 수 있음
+ */
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
 
-        int gcd = getGCD(n, m);
-        int lcm = getLCM(n, m, gcd);
-
-        answer[0] = gcd;
-        answer[1] = lcm;
+        answer[0] = getGCD(n, m);
+        answer[1] = getLCM(n, m, getGCD(n, m));
         return answer;
     }
 
-    public Set<Integer> addIntToSet(int x) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 1; i <= x; i++) {
-            if (x % i == 0)
-                set.add(x / i);
-        }
-        return set;
-    }
-
     public int getGCD(int n, int m) {
-        Set<Integer> a = addIntToSet(n);
-        Set<Integer> b = addIntToSet(m);
-        Set<Integer> intersection = new HashSet<>(a);
-
-        intersection.retainAll(b);
-        int gcd = Integer.MIN_VALUE;
-        for (int element : intersection) {
-            if (gcd < element)
-                gcd = element;
+        if (m == 0) {
+            return n;
         }
-        return gcd;
+        return getGCD(m, n % m);
     }
 
     public int getLCM(int n, int m, int gcd) {
-        return (n * m) / gcd;
+        return n * m / gcd;
     }
 }
