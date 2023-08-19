ecdd3f6ee76e6b06ac604aabb9d2a37276723b48
junhyun1001 <90903322+junhyun1001@users.noreply.github.com>
2023-08-19 16:50:45 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,21 @@
-public class Solution {
+class Solution {
     public int solution(int n) {
+        int result = 0;
+        for (int i = 10; i <= 100000000; i *= 10) {
+            if (n / i >= 1 && n / i < 10) {
+                result += n / i;
+                result = getSum(n, i) + result;
+            } else if (n < 10) {
+                return n;
+            }
+        }
+        return result;
+    }
+
+    public int getSum(int n, int i) {
         int sum = 0;
-        while (true) {
-            sum += n % 10;
-            if (n / 10 == 0) break;
-            n /= 10;
+        for (int j = i / 10; j >= 1; j /= 10) {
+            sum += n % (j * 10) / j;
         }
         return sum;
     }
