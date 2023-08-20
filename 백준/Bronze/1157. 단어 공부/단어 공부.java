847a62ea73dc073e0cfd3e82f0b4b3ff48bdaef3
junhyun1001 <90903322+junhyun1001@users.noreply.github.com>
2023-08-21 02:01:54 +0900

    Update 단어 공부.java

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class BaekJoon {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,6 +11,7 @@ public class Main {
         String s = br.readLine().toUpperCase();
 
         Map<Character, Integer> map = new HashMap<>();
+        // map의 특성을 이용해서 중복된 key는 덮어씌우고 value + 1 
         for (int i = 0; i < s.length(); i++) {
             if (!map.containsKey(s.charAt(i))) {
                 map.put(s.charAt(i), 1);
@@ -22,11 +23,12 @@ public class Main {
 
         char c = ' ';
         int max = Integer.MIN_VALUE;
+        // value에서 최대값을 찾고 해당 문자열 출력
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
             if (max < entry.getValue()) {
                 c = entry.getKey();
                 max = entry.getValue();
-            } else if (max == entry.getValue()) {
+            } else if (max == entry.getValue()) { // 최대값이 같은게 있으면 ? 출력
                 c = '?';
             }
         }
