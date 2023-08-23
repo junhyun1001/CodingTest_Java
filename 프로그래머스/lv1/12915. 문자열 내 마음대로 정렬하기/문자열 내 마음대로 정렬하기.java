36b8b9482d50070bc2b6ccd24004819d3d6b03c6
junhyun1001 <90903322+junhyun1001@users.noreply.github.com>
2023-08-23 23:26:42 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.89 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -4,30 +4,15 @@ import java.util.Comparator;
 class Solution {
     public String[] solution(String[] strs, int n) {
 
-        Arrays.sort(strs, new IndexBasedComparator(n));
+        Arrays.sort(strs, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int result = Character.compare(s1.charAt(n), s2.charAt(n));
+                if(result == 0) return s1.compareTo(s2);
+                else return result;
+            }
+        });
 
         return strs;
-    }
-
-}
-
-class IndexBasedComparator implements Comparator<String> {
-    private final int n;
-
-    public IndexBasedComparator(int n) {
-        this.n = n;
-    }
-
-    @Override
-    public int compare(String o1, String o2) {
-        // n번째 인덱스 값 비교
-        int result = Character.compare(o1.charAt(n), o2.charAt(n));
-
-        // n번째 문자가 같을 때 사전순 정렬을 리턴
-        if (result == 0) {
-            return o1.compareTo(o2);
-        }
-
-        return result;
     }
 }
