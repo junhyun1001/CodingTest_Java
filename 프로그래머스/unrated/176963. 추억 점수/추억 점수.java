7bf20b0077c17280d92d7ee1e028046624b08984
junhyun1001 <90903322+junhyun1001@users.noreply.github.com>
2023-08-28 18:34:09 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        
+        int[] answer = new int[photo.length]; 
+        for(int i = 0; i < photo.length; i++) {
+            for(int j = 0; j < photo[i].length; j++) {
+                if (map.get(photo[i][j]) == null) answer[i] += 0;
+                else answer[i] += map.get(photo[i][j]);
+            }
+        }
+        return answer;
+    }
+}
