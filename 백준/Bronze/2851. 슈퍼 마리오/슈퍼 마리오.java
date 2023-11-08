c917099304c7a2e9d409f1270b2f881e94f1e1e2
samdaeoback <90903322+junhyun1001@users.noreply.github.com>
2023-11-09 01:02:42 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 76 ms, Memory: 11336 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -3,20 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int i,d,ans;
-		int[] arr = new int[11];
-		d = 100;
-		ans = 0;
-		for(i = 1; i <= 10; i++) {
-			arr[i] = arr[i - 1] + Integer.parseInt(br.readLine());
-			if(d>=Math.abs(arr[i] - 100)) {
-				d = Math.abs(arr[i] - 100);
-				ans = arr[i];
-			}
-		}
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int answer = arr[0];
+        for (int i = 1; i < 10; i++) {
+            arr[i] += arr[i - 1];
+
+            if (arr[i] > 100) {
+                int abs1 = Math.abs(arr[i] - 100);
+                int abs2 = Math.abs(arr[i - 1] - 100);
+                if (abs1 > abs2) {
+                    answer = arr[i - 1];
+                } else {
+                    answer = arr[i];
+                }
+                break;
+            } else if (arr[i] < 100){
+                answer = arr[i];
+            }
+
+        }
+
+        System.out.println(answer);
+
+
+    }
 }
