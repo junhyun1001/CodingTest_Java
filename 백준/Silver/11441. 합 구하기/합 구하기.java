2dbaa8b5881eda408840de3af584b25857624127
samdaeoback <90903322+junhyun1001@users.noreply.github.com>
2023-12-31 22:59:19 +0900

    [Silver III] Title: 합 구하기, Time: 2128 ms, Memory: 82980 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int[] arr = new int[n + 1];
+        arr[1] = Integer.parseInt(st.nextToken());
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println(arr[b] - arr[a]);
+        }
+
+    }
+}
